ad3d1a38ac627a2e8a118a79a2e5d65ae25f2e26
Rich Worsley <rworsl01@dcs.bbk.ac.uk>
2020-12-21 21:59:49 +0000

    first page

--- TournamentPro/MainWindow.xaml.cs
+++ TournamentPro/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace TournamentPro
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> CategoryAPlayers = new List<string>();
+        List<string> CategoryBPlayers = new List<string>();
+        List<string> CategoryCPlayers = new List<string>();
         public MainWindow()
         {
+            Info.Text = "This is primarily set up to handle 24 players. The hope is that it will be able to handle more or less without throwing any erros, but don't get your hopes up.";
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(CategoryA.Items.Count != CategoryB.Items.Count || CategoryA.Items.Count != CategoryC.Items.Count)
+            {
+                TextDebug.Text = "Uneven number of items";
+            }
+            else
+            {
+                TextDebug.Text = "Even number of items";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            CategoryA.Items.Remove(CategoryA.SelectedItem);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var nameToAdd = CAName.Text;
+            CategoryA.Items.Add(nameToAdd);
+            CAName.Clear();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var nameToAdd = CAName.Text;
+            CategoryB.Items.Add(nameToAdd);
+            CAName.Clear();
         }
     }
 }
